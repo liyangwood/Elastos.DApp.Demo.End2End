3d269f2549f4b54672f84e20854d3330fe77e989
jacky.li <jacky.li@accenture.com>
2018-07-14 02:56:44 +0800

    remove files

--- platforms/android/app/src/main/java/com/elastos/spvcore/ISubWalletCallback.java
+++
@@ -1,8 +0,0 @@
-
-package com.elastos.spvcore;
-
-
-public interface ISubWalletCallback {
-    //public void OnBalanceChanged(String address,double oldAmount,double newAmount);
-    public void OnTransactionStatusChanged(String txId, String status, String desc,int confirms);
-}
